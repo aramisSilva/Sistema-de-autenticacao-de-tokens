975c9cb75fd91dd35d2a2961119a234cdaee4a76
aramisdev <aods@ic.ufal.br>
2022-05-06 17:21:12 -0300

    Sistema de Login

---
+++ frontend/src/Componentes/Item/ItemComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ItemComponent(props){
+    const status = props.status;
+    return <li> { props.name } 
+    <div>Status: {status ? <div>Finalizado</div> : <div>Nao Finalizado</div>} </div>
+    </li>
+
+}
